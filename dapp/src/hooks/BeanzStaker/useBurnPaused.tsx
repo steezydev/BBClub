81e5c33db10f98c88389d228b877dcae69690122
steezy2401 <swggNuko.dev@gmail.com>
2022-07-15 13:26:41 +0300

    feat(dapp): :sparkles: added paused hooks

---
+++ dapp/src/hooks/BeanzStaker/useBurnPaused.tsx
@@ -0,0 +1,19 @@
+import { useCall } from '@usedapp/core';
+
+import { BeanzStakerContract } from '@/lib/contractsProvider';
+
+const useBurnPaused = (): [boolean | undefined, Error | undefined] => {
+  const { value, error } =
+    useCall({
+      contract: BeanzStakerContract,
+      method: 'burnPaused',
+      args: [],
+    }) ?? {};
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return [value?.[0], error];
+};
+
+export default useBurnPaused;
