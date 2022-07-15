f129a2751bc932b756c3f4f34273d6d3440f2434
steezy2401 <swggNuko.dev@gmail.com>
2022-07-15 14:39:32 +0300

    feat(dapp): :sparkles: burn info

--- dapp/src/components/BurnInfo/BurnInfo.tsx
+++ dapp/src/components/BurnInfo/BurnInfo.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import useClaimRewards from '@/hooks/BeanzStaker/useClaimRewards';
 import useUserStakeInfo from '@/hooks/BeanzStaker/useUserStakeInfo';
 
 interface StakeInfoProps {
@@ -9,27 +8,18 @@ interface StakeInfoProps {
 
 export default function StakeInfo({ address }: StakeInfoProps) {
   const [stakeInfo] = useUserStakeInfo(address);
-  const [claimRewards] = useClaimRewards();
 
   return (
     <div className='mb-16 flex flex-col items-center gap-8 text-xl md:flex-row md:items-start md:justify-around md:gap-0'>
       <div className='flex flex-col items-center justify-center'>
-        <span className='text-3xl'>{stakeInfo.amountStaked ?? '-'}</span>
-        <span className='font-secondary'>beanz staked</span>
+        <span className='text-3xl'>{stakeInfo.amountBurned ?? '-'}</span>
+        <span className='font-secondary'>beanz burned</span>
       </div>
       <div className='flex flex-col items-center justify-center'>
         <span className='relative'>
           <span className='text-3xl'>{stakeInfo.reward ?? '-'}</span>
         </span>
         <span className='font-secondary'>accumulated $BBC</span>
-        {stakeInfo.reward ?? 0 > 0 ? (
-          <span
-            onClick={() => claimRewards()}
-            className='nes-text is-primary nes-pointer mt-3 text-sm'
-          >
-            Claim
-          </span>
-        ) : null}
       </div>
     </div>
   );
--- dapp/src/pages/burn.tsx
+++ dapp/src/pages/burn.tsx
@@ -1,16 +1,84 @@
+import { useEthers } from '@usedapp/core';
 import * as React from 'react';
+import { useEffect, useState } from 'react';
+
+import { getNftsData } from '@/lib/utils/nftData';
+import useIsApprovedForAll from '@/hooks/BeanzDeployer/useIsApprovedForAll';
+import useWalletOfOwner from '@/hooks/BeanzDeployer/useWalletOfOwner';
+import useBurnPaused from '@/hooks/BeanzStaker/useBurnPaused';
+import useStake from '@/hooks/BeanzStaker/useStake';
 
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
+import Stake from '@/components/Stake/Stake';
+import StakeInfo from '@/components/StakeInfo/StakeInfo';
+
+import { TNftData } from '@/types/nft.types';
 
 export default function BurnPage() {
+  const { account } = useEthers();
+  const [mintedTokens] = useWalletOfOwner(account);
+
+  const [mintedTokensData, setMintedTokensData] = useState<TNftData[]>();
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
+
+  const [isApprovedForAll] = useIsApprovedForAll(account);
+  const [burnPaused] = useBurnPaused();
+
+  const [stake, stakeState] = useStake();
+
+  useEffect(() => {
+    if (mintedTokens != undefined) {
+      getNftsData(mintedTokens).then((data) => {
+        setMintedTokensData(data);
+      });
+    }
+  }, [mintedTokens]);
+
+  const handleStake = async (ids: number[]) => {
+    if (ids.length <= 0) return undefined;
+    // call to stake()
+    stake(ids);
+  };
+
+  useEffect(() => {
+    if (!isApprovedForAll && isApprovedForAll != undefined)
+      setErrorMessage('Beanz need approval...');
+    if (burnPaused ?? false) setErrorMessage('The burning is paused...');
+  }, [burnPaused, isApprovedForAll]);
+
   return (
     <Layout>
       <Seo templateTitle='Burn' />
 
       <main>
         <section className=''>
-          <div className='layout min-h-screen py-20'></div>
+          <div className='layout min-h-screen py-20'>
+            <StakeInfo address={account} />
+            <div>
+              <Stake
+                title='Burn'
+                description='Select which Beanz to burn'
+                actionName='Burn'
+                data={mintedTokensData}
+                actionVariant='error'
+                action={handleStake}
+                disableAction={
+                  (stakeState.status != 'None' &&
+                    stakeState.status != 'Success' &&
+                    stakeState.status != 'Exception') ||
+                  (burnPaused ?? false)
+                }
+                emptyMessage='You don`t have any Beanz in your wallet :('
+                errorMessage={errorMessage}
+                needsApproval={
+                  !isApprovedForAll && isApprovedForAll != undefined
+                }
+              />
+            </div>
+          </div>
         </section>
       </main>
     </Layout>
